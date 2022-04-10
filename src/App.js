86c35babcfdfa17b6ce71d224716937bed525758
Lalith Yagnavalkya <lalluyagnavalkya.t@gmail.com>
2022-04-10 14:26:17 +0530

    inital commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,31 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+
+import firebase from "firebase/app";
+import "firebase/firestore";
+import "firebase/auth";
+
+import { initializeApp } from "firebase/app";
+
+import { useAuthState } from "react-firebase-hooks/auth";
+import { useCollectionData } from "react-firebase-hooks/firestore";
+
+firebase.initializeApp({
+  apiKey: "AIzaSyC9eDo1f-BwWn9-5g5UQXPM1ST2xPlXj78",
+  authDomain: "looseleaftassignment.firebaseapp.com",
+  projectId: "looseleaftassignment",
+  storageBucket: "looseleaftassignment.appspot.com",
+  messagingSenderId: "406940863270",
+  appId: "1:406940863270:web:ebfc4e8cdc6f9509968fe7",
+  measurementId: "G-4ZCMNJB6F5",
+});
+
+const auth = firebase.auth();
+const firestore = firebase.firestore();
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  const [user] = useAuthState(auth);
+  return <div className="App"></div>;
 }
 
 export default App;
